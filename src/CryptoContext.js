c60ab2394deb38f2b6db9448eb605acb9e57ffed
Rohit Suryawanshi <47253310+rohitsurya99@users.noreply.github.com>
2022-03-06 12:05:28 +0530

    useless deleted

--- src/CryptoContext.js
+++ src/CryptoContext.js
@@ -1,9 +1,8 @@
-import React, { createContext, useContext, useEffect, useState } from 'react'
-
+import React, {  createContext, useContext, useEffect, useState } from 'react'
 
 const Crypto = createContext();
 
-const CryptoContext = ({ children }) => {
+const CryptoContext = ({children}) => {
 
     const [currency, setCurrency] = useState("INR");
     const [symbol, setSymbol] = useState("₹");
@@ -13,11 +12,15 @@ const CryptoContext = ({ children }) => {
         else if (currency ==="USD") setSymbol("$");
     },[currency])
 
-  return<Crypto.Provider value={{currency,symbol,setCurrency}}>{children}</Crypto.Provider>
+  return (
+    <Crypto.Provider value={{currency,symbol,setCurrency}}>
+      {children}
+    </Crypto.Provider>
+  )
 }
 
 export default CryptoContext;
 
-export const CryptoState = ()=>{
-    return useContext(Crypto);
-}
+export const CryptoState = (()=>{
+   return useContext(Crypto);
+});
